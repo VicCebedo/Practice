df618d26a6eee53a662251883a831e1dac6f14e5
Victorio Cebedo II <cebedo.vii@gmail.com>
2019-07-08 09:53:10 +0800

    Dynamic programming practice.

---
+++ vcc-practice/src/com/cebedo/hackerrank/algorithms/dynamic/CoinChange.java
@@ -0,0 +1,67 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Vic Cebedo <cebedo.vii@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.cebedo.hackerrank.algorithms.dynamic;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vic Cebedo <cebedo.vii@gmail.com>
+ */
+public class CoinChange {
+
+    static long getWays(long n, long[] c) {
+        int len = (int) n + 1;
+        long[] combinations = new long[len];
+        combinations[0] = 1;
+
+        for (long coin : c) {
+            for (int i = 1; i < combinations.length; i++) {
+                if (i >= coin) {
+                    combinations[i] += combinations[i - (int) coin];
+                }
+            }
+        }
+        return combinations[(int) n];
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String[] nm = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+        long[] c = new long[m];
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int i = 0; i < m; i++) {
+            long cItem = Long.parseLong(cItems[i]);
+            c[i] = cItem;
+        }
+        long ways = getWays(n, c);
+        scanner.close();
+    }
+}
