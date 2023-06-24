60f6bbde84b9634801724b2f8a1f0ef08f68add9
Vic Cebedo <vcc2@tuta.io>
2023-06-24 09:54:12 +0800

    Added final code from tour of heroes.

--- vcc-angular/angular-tour-of-heroes/src/app/hero.service.ts
+++ vcc-angular/angular-tour-of-heroes/src/app/hero.service.ts
@@ -13,6 +13,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
   constructor(
     private messageService: MessageService,
     private http: HttpClient
@@ -28,6 +32,51 @@ export class HeroService {
       .pipe(
         tap(_ => this.log(`fetched hero id=${id}`)),
         catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+      .pipe(
+        tap(x => {
+          x.length
+            ? this.log(`found heroes matching "${term}"`)
+            : this.log(`no heroes matching "${term}"`)
+        }),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      );
+  }
+
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`deleted hero id=${id}`)),
+        catchError(this.handleError<Hero>('deleteHero'))
+      );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(
+      this.heroesUrl,
+      hero,
+      this.httpOptions
+    )
+      .pipe(
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
       );
   }
 
--- vcc-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ vcc-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -22,6 +22,25 @@ export class HeroesComponent {
     this.getHeroes();
   }
 
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => {
+      h !== hero
+    });
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => {
